35f7f12b49214cde182bc0122d38d7e852001a57
pekay3058-jpg <pekay3058@gmail.com>
2025-09-18 09:04:18 +0000

    firebase config.js

--- firebase config.js
+++ firebase config.js
@@ -1,19 +1,13 @@
-
-// firebase-config.js
-var firebaseConfig = {
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
   apiKey: "AIzaSyCB3TOwlnDAdh_x5NvLSCyORX_6oZbsZGc",
   authDomain: "falaa-freebies.firebaseapp.com",
   projectId: "falaa-freebies",
-  storageBucket: "falaa-freebies.appspot.com",
+  storageBucket: "falaa-freebies.firebasestorage.app",
   messagingSenderId: "842425176920",
   appId: "1:842425176920:web:aa538c8212f12dd41fcb2d",
   measurementId: "G-YQFW6R14L6"
 };
-
-// Initialize Firebase
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
-}
 
 // Services
 const auth = firebase.auth();
