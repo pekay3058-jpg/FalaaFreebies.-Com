808090eaf691b0cb80916cc32102ae0b1d58023d
Ransford Essel <pekay3058@gmail.com>
2025-09-21 09:05:58 +0000

    auth.js

--- auth.js
+++ auth.js
@@ -1,3 +1,14 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCB3TOwlnDAdh_x5NvLSCyORX_6oZbsZGc",
+  authDomain: "falaa-freebies.firebaseapp.com",
+  projectId: "falaa-freebies",
+  storageBucket: "falaa-freebies.firebasestorage.app",
+  messagingSenderId: "842425176920",
+  appId: "1:842425176920:web:aa538c8212f12dd41fcb2d",
+  measurementId: "G-YQFW6R14L6"
+};
+
 // auth.js
 import { auth, db } from "./firebase-init.js"; // make sure path is correct
 import { 
